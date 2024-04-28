e59d6e78ab4ae222e965dbdefe7b49c8a29abca0
WHITEBEARD\nisha <nishantsahai221@gmail.com>
2024-04-28 21:21:44 +0530

    added advice genres on signup and made it a required field

--- Controllers/users.js
+++ Controllers/users.js
@@ -3,18 +3,26 @@ const OtpModel = require("../Modals/otpModel");
 const { getToken } = require("../Services/authentication");
 
 const createAccount = async (req, res) => {
-  const { username, password, emailId } = req.body;
-  if (!username || !password || !emailId) {
+  try {
+    const { username, password, emailId, adviceGenre } = req.body;
+    if (!username || !password || !emailId || !adviceGenre) {
+      return res
+        .status(400)
+        .json({
+          message: "either username, pass or email or genres are missing",
+        });
+    }
+    await Users.create({ username, password, emailId, adviceGenre });
+    return res.status(200).json({ message: "Account created!" });
+  } catch (err) {
     return res
-      .status(400)
-      .json({ message: "either username, pass or email is missing" });
+      .status(500)
+      .json({ success: true, message: "some error occured : " + err.message });
   }
-  await Users.create({ username, password, emailId });
-  return res.status(200).json({ message: "Account created!" });
 };
 
 const loginAccount = async (req, res) => {
-  try{
+  try {
     const { username, password } = req.body;
     if (!username || !password) {
       return res
@@ -25,42 +33,41 @@ const loginAccount = async (req, res) => {
     if (resultedUser) {
       const access_token = getToken(resultedUser);
       return res.status(200).json({ access_token, message: "success" });
-    }else{
-      return res.status(404).json({success: false, message: "User does not exist"})
+    } else {
+      return res
+        .status(404)
+        .json({ success: false, message: "User does not exist" });
     }
-  } catch(err){
-    return res.status(500).json({success: false, message: err.message})
+  } catch (err) {
+    return res.status(500).json({ success: false, message: err.message });
   }
-  
 };
 
 const changePass = async (req, res) => {
-  try{
+  try {
     const { emailId, password, otp } = req.body;
     if (!password) {
       return res.status(400).json({ message: "password is missing" });
     } else if (!emailId) {
       return res.status(400).json({ message: "emailId is missing" });
     }
-    const otpVerification = await OtpModel.findOne({ emailId , otp});
-    console.log('a :', otpVerification)
+    const otpVerification = await OtpModel.findOne({ emailId, otp });
+    console.log("a :", otpVerification);
     if (otpVerification.verified) {
-      console.log('otp verification :', otpVerification)
-      await OtpModel.findOneAndDelete({emailId, otp})
-      await Users.findOneAndUpdate(
-        { emailId },
-        { password }
-      );
+      console.log("otp verification :", otpVerification);
+      await OtpModel.findOneAndDelete({ emailId, otp });
+      await Users.findOneAndUpdate({ emailId }, { password });
       return res
         .status(200)
         .json({ success: true, message: "password updated successfully" });
     } else {
-      return res.status(404).json({ success: false, message: "otp not verified" });
+      return res
+        .status(404)
+        .json({ success: false, message: "otp not verified" });
     }
-  } catch(err){
-    return res.status(500).json({success: false, message: err.message})
+  } catch (err) {
+    return res.status(500).json({ success: false, message: err.message });
   }
-  
 };
 
 module.exports = {
--- Modals/users.js
+++ Modals/users.js
@@ -1,12 +1,52 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
-    username: {type: String, required: true, unique: true},
-    password: {type: String, required: true},
-    emailId: {type: String, required: true, unique: true}
-}, {TimeStamps: true})
+const allGenres = [
+  "Health & Fitness",
+  "Relationships",
+  "Psycology",
+  "Financial",
+  "Career",
+  "Education",
+  "Entrepreneurship",
+  "Politics",
+  "Diet",
+  "Fashion",
+  "Family",
+  "Pets",
+  "Medicine",
+  "Cooking",
+  "Movies",
+  "Music",
+  "Games",
+  "Vehicals",
+  "Plants",
+  "Yoga",
+  "Culture",
+  "Dancing",
+  "Spiritual",
+  "Technology",
+  "Coding",
+  "Traveling",
+];
 
-const Users = mongoose.model('users', UserSchema)
+const UserSchema = new mongoose.Schema(
+  {
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    emailId: { type: String, required: true, unique: true },
+    adviceGenre: {
+      type: [
+        {
+          type: String,
+          enum: allGenres, // This ensures that only values from predefinedStrings are allowed
+        },
+      ],
+      required: true,
+    },
+  },
+  { TimeStamps: true }
+);
 
+const Users = mongoose.model("users", UserSchema);
 
-module.exports = Users
+module.exports = Users;
